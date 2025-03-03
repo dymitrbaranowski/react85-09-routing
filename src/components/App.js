5b941169095fab41f3149d9c34970e08bd8fc750
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-03-03 14:07:19 +0200

    fox2

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,38 @@
 import { Toaster } from 'react-hot-toast';
 import { GlobalStyle } from './GlobalStyle';
 import { Layout } from './Layout';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useSearchParams } from 'react-router-dom';
 import QuizzesPages from 'pages/QuizzesPage';
 import CreateQuizPage from 'pages/CreateQuizPage';
 import QuizDetailsPage from 'pages/QuizDetailsPage';
+import { useEffect, useState } from 'react';
 
 export const App = () => {
+  // const [user, setUser] = useState(null);
+  // const [serachParams, setSearchParams] = useSearchParams();
+  // const userName = serachParams.get('userName') ?? '';
+  // const password = serachParams.get('password') ?? '';
+
+  // useEffect(() => {
+  //   if (userName && password === '') return;
+
+  //   async function login() {
+  //     const user = await FakeAPI(userName, password);
+  //     setUser(user);
+  //   }
+  //   login();
+  // }, [userName, password]);
+
+  // const handleSubmit = e => {
+  //   e.preventDefault();
+  //   const form = e.currentTarget;
+  //   setSearchParams({
+  //     userName: form.elements.userName.value,
+  //     password: form.elements.password.value,
+  //   });
+  //   form.reset();
+  // };
+
   return (
     <>
       <Routes>
